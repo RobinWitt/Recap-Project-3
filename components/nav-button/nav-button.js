696db5faffc8bbc93157a518f933db3c9ce7ff5d
iris herholz <irisherholz@MacBook-Pro-von-iris.local>
2022-12-12 15:29:23 +0100

    add ButtonJS

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,7 +1,17 @@
-export function pageDown(page) {
-  return page--;
+import { navigation } from "../index.js";
+
+export function createPreviousButton() {
+  const newButton = document.createElement("button");
+  newButton.classList.add("button button--prev");
+  newButton.setAttribute("data-js", "button-prev");
+  newButton.textContent = "previous";
+  navigation.append(newButton);
 }
 
-export function pageUp(page) {
-  return page++;
+export function createNextButton() {
+  const newButton = document.createElement("button");
+  newButton.classList.add("button button--next");
+  newButton.setAttribute("data-js", "button-next");
+  newButton.textContent = "next";
+  navigation.append(newButton);
 }
